81cc9f9116dfedb87393a3b7a6817ba36934493e
Adrian Dorado Gallardo <adriaan0220@gmail.com>
2025-12-09 15:57:54 +0000

    arregla el error y ponme la carpeta out

--- next.config.ts
+++ next.config.ts
@@ -54,7 +54,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'static.wixstatic.com',
         port: '',
         pathname: '/**',
