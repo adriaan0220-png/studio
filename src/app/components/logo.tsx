e1319971520fb1140c08dd6473dd6dffb2dd537b
Adrian Dorado Gallardo <adriaan0220@gmail.com>
2025-12-23 05:38:23 +0000

    File changes

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-semibold group">
       <Image
-        src="https://storage.googleapis.com/debbie-419815.appspot.com/uploads/prod/image/34a0247659a8425d97f259c1186b36ab/tetiko.png"
+        src="https://storage.googleapis.com/debbie-419815.appspot.com/uploads/prod/image/34a0247659a8425d97f259c1186b36ab/logitrans.png"
         alt="Logitrans Logo"
         width={150}
         height={40}
