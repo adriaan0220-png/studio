9bcbc5d9135c56ca3331b6583ac8c8a6f847d3d8
Adrian Dorado Gallardo <adriaan0220@gmail.com>
2026-01-13 14:19:30 +0000

    cambia de color el titulo de nuestros servicios

--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { PageHeader } from '@/app/components/page-header';
@@ -92,7 +93,7 @@ export default function ServiciosPage() {
   return (
     <div className="p-8 flex-1 bg-green-50">
       <PageHeader
-        title="Nuestros Servicios"
+        title={<span className="text-secondary">Nuestros Servicios</span>}
         description="Descubra la gama de servicios de transporte especializado que ofrecemos."
       />
       <div className="space-y-8">
