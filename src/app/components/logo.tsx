00e2fccc5acad2e2113dcbfc680725e632705918
Adrian Dorado Gallardo <adriaan0220@gmail.com>
2026-01-19 19:30:55 +0000

    ponme la foto Dorado_Adrián_Logo.png como foto de perfil de la pagina we

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-semibold group">
       <Image
-        src="https://storage.googleapis.com/debbie-419815.appspot.com/uploads/prod/image/33215be1-8b01-4475-a01b-c744c68066f1/tetiko-trans-logo.png"
+        src="/Dorado_Adrián_Logo.png"
         alt="Tetiko Trans Logo"
         width={180}
         height={40}
